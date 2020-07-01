dd9e7f92ca41a64d1cc7783f630f7f32312a769b
Daniel <djisenberg2015@gmail.com>
2020-07-01 12:19:59 -0400

    modified walkerObject.py so that in walkUniform(), positionList contains a list of integers, where before it contained a list of arrays which each contained a single integer (This was making matplotlibs histogram break, with each bin containing exactly 1 element, plotting what should have been the x axis on the y axis and vise versa, and making each bin a different color, and making the total number of bins equal to the total number of runs, even when manually setting the number of bins to a lower or higher number. I mostly understand why this was happening.)
    Also changed walkerObject.py so that its position and start position are stored as numbers rather than a vector. Additionally removed import of vpython because it wasnt being used and made using the debugger a pain.
    
    Added explanations for some variables in main_randomWalk.py, added ability to plot a histogram of the average position of each run for 'numberOfRuns' runs.

--- .idea/main_randomWalk.py
+++ .idea/main_randomWalk.py
@@ -1,34 +1,48 @@
 import numpy as np
-from vpython import *
 import walkerObject
 import matplotlib.pyplot as plt
 
-walkLength = 100
-minStepSize = -1
-maxStepSize = 1
-stepRange = 1
+startPosition = 0  # the starting position of the walker (the walkers position is reset to this after each run)
+walkLength = 1000  # the number of steps taken by the walker object for each run
+numberOfRuns = 5000  # the number of runs the walker will perform
+minStepSize = -1  # the lower bound of the possible step size using walkerObject.walkUniform()
+maxStepSize = 1   # the upper bound of the possible step size using walkerObject.walkUniform()
+stepRange = 1     # the magnitude of the min/max step size using walkerObject.walkRange()
 
-startVector = vector(0, 0, 0)
-drunk = walkerObject.walker(startVector, walkLength)
+averageList = []  # list containing the average position of the walker for each run
+runList = []  # list for holding integers from 0 to numberOfRuns, used for plotting purposes.
 
-drunk.walkUniform(minStepSize, maxStepSize)
-print("uniform")
-print(np.average(drunk.positionList))
-plt.plot(drunk.stepList, drunk.positionList)
-plt.plot(drunk.stepList, drunk.positionList, 'b.')
-plt.xlabel("Step number")
-plt.ylabel("Walker position")
-plt.grid(True)
+
+drunk = walkerObject.walker(startPosition, walkLength)
+
+
+for i in range(0, numberOfRuns):
+    drunk.walkUniform(minStepSize, maxStepSize)
+    positionSum = sum(drunk.positionList)
+    average = positionSum / len(drunk.positionList)
+    #print("average position for run " + str(i) + " is " + str(average))
+    averageList.append(average)
+    runList.append(i)
+    drunk.reset()
+
+    """
+    plt.plot(drunk.stepList, drunk.positionList)
+    plt.plot(drunk.stepList, drunk.positionList, 'b.')
+    plt.xlabel("Step number")
+    plt.ylabel("Walker position")
+    plt.show()
+    """
+
+"""
+plt.plot(runList, averageList)
+plt.plot(runList, averageList, "b.")
+plt.ylabel("Average run position")
+plt.xlabel("Run number")
 plt.show()
+"""
 
-drunk.reset()
-
-drunk.walkRange(stepRange)
-print("range")
-print(np.average(drunk.positionList))
-plt.plot(drunk.stepList, drunk.positionList)
-plt.plot(drunk.stepList, drunk.positionList, 'b.')
-plt.xlabel("Step number")
-plt.ylabel("Walker position")
-plt.grid(True)
+plt.hist(averageList, 100)
+plt.suptitle("histogram of avarage position after each run")
+plt.xlabel("average position after run")
+plt.ylabel("number of runs in respective range")
 plt.show()
--- walkerObject.py
+++ walkerObject.py
@@ -2,25 +2,25 @@ import numpy as np
 
 
 class walker:
-    def __init__(self, startPositionVector, walkSize):
+    def __init__(self, startPosition, walkSize):
         self.walkSize = walkSize
-        self.startPosition = startPositionVector
-        self.position = startPositionVector
+        self.startPosition = startPosition
+        self.position = startPosition
         self.positionList = []
         self.stepList = []
 
     def walkUniform(self, minStep, maxStep):
+        randomStepList = np.random.uniform(minStep, maxStep, self.walkSize)
         for i in range(0, self.walkSize):
             self.stepList.append(i)
-            self.positionList.append(self.position.x)
-            self.position.x = self.position.x + np.random.uniform(minStep, maxStep, 1)
+            self.positionList.append(self.position)
+            self.position = self.position + randomStepList[i]
 
     def walkRange(self, stepRange):
         for i in range(0, self.walkSize):
             self.stepList.append(i)
-            self.positionList.append(self.position.x)
-            self.position.x = self.position.x + (2.0 * stepRange * (np.random.random_sample() - 0.5))
-        #self.positionList = 2.0 * stepRange * (np.random.random_sample(self.walkSize) - 0.5)
+            self.positionList.append(self.position)
+            self.position = self.position + (2.0 * stepRange * (np.random.random_sample() - 0.5))
 
     def reset(self):
         self.positionList.clear()
