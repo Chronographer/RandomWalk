a9e8a2cf05bddd6950a8a64e44d5d39fca343eb1
Daniel <djisenberg2015@gmail.com>
2020-07-01 11:12:17 -0400

    added walker.reset() function to clear existing walk data before starting a new walk with the same walkerObject. Added new type of random walk function that generates random steps based on a range centered on zero instead of between 2 given numbers.

--- .idea/main_randomWalk.py
+++ .idea/main_randomWalk.py
@@ -4,13 +4,27 @@ import walkerObject
 import matplotlib.pyplot as plt
 
 walkLength = 100
-minStep = -1
-maxStep = 1
+minStepSize = -1
+maxStepSize = 1
+stepRange = 1
 
 startVector = vector(0, 0, 0)
 drunk = walkerObject.walker(startVector, walkLength)
-drunk.walk(minStep, maxStep)
 
+drunk.walkUniform(minStepSize, maxStepSize)
+print("uniform")
+print(np.average(drunk.positionList))
+plt.plot(drunk.stepList, drunk.positionList)
+plt.plot(drunk.stepList, drunk.positionList, 'b.')
+plt.xlabel("Step number")
+plt.ylabel("Walker position")
+plt.grid(True)
+plt.show()
+
+drunk.reset()
+
+drunk.walkRange(stepRange)
+print("range")
 print(np.average(drunk.positionList))
 plt.plot(drunk.stepList, drunk.positionList)
 plt.plot(drunk.stepList, drunk.positionList, 'b.')
--- walkerObject.py
+++ walkerObject.py
@@ -3,14 +3,26 @@ import numpy as np
 
 class walker:
     def __init__(self, startPositionVector, walkSize):
-        self.position = startPositionVector
         self.walkSize = walkSize
-
+        self.startPosition = startPositionVector
+        self.position = startPositionVector
         self.positionList = []
         self.stepList = []
 
-    def walk(self, minStep, maxStep):
+    def walkUniform(self, minStep, maxStep):
         for i in range(0, self.walkSize):
             self.stepList.append(i)
             self.positionList.append(self.position.x)
             self.position.x = self.position.x + np.random.uniform(minStep, maxStep, 1)
+
+    def walkRange(self, stepRange):
+        for i in range(0, self.walkSize):
+            self.stepList.append(i)
+            self.positionList.append(self.position.x)
+            self.position.x = self.position.x + (2.0 * stepRange * (np.random.random_sample() - 0.5))
+        #self.positionList = 2.0 * stepRange * (np.random.random_sample(self.walkSize) - 0.5)
+
+    def reset(self):
+        self.positionList.clear()
+        self.stepList.clear()
+        self.position = self.startPosition
